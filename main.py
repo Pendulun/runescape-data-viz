f5fbe24670e0083fa06276bc752bd8237bbdd8c8
Daniel Campos <danielcampos14@hotmail.com.br>
2024-09-26 12:59:42 -0300

    add doc metadata

--- main.py
+++ main.py
@@ -35,9 +35,18 @@ async def read_file(file_path: str):
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[str | None,
-                                  Query(min_length=3, max_length=50)] = None,
-                    list_param : Annotated[Union[list[str], None], Query()] = None):
+async def read_items(q: Annotated[
+    str | None,
+    Query(
+        min_length=3,
+        max_length=50,
+        title="Query string",
+        description=
+        "Query string for the items to search in the database that have a good match",
+        deprecated=True,
+    )] = None,
+                     list_param: Annotated[Union[list[str], None],
+                                           Query()] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
