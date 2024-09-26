a7c190f2aa6d6118cea0e97c625f06a94d53b09b
Daniel Campos <danielcampos14@hotmail.com.br>
2024-09-26 12:46:40 -0300

    query params validations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from enum import Enum
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -10,10 +11,12 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -25,9 +28,20 @@ async def get_model(model_name: ModelName):
 
     return {"model_name": model_name, "message": "Have some residuals"}
 
+
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None,
+                                  Query(min_length=3, max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: str | None = None, short: bool = False):
@@ -35,22 +49,27 @@ async def read_item(item_id: str, q: str | None = None, short: bool = False):
     if q:
         item.update({"q": q})
     if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+        item.update({
+            "description":
+            "This is an amazing item that has a long description"
+        })
     return item
 
+
 @app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, needy:str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id, 'needy':needy}
+async def read_user_item(user_id: int,
+                         item_id: str,
+                         needy: str,
+                         q: str | None = None,
+                         short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id, 'needy': needy}
     if q:
         item.update({"q": q})
     if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+        item.update({
+            "description":
+            "This is an amazing item that has a long description"
+        })
     return item
 
 
@@ -60,6 +79,7 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
+
 @app.post("/items/")
 async def create_item(item: Item):
     item_dict = item.model_dump()
@@ -67,6 +87,7 @@ async def create_item(item: Item):
         price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
+
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item, q: str | None = None):
