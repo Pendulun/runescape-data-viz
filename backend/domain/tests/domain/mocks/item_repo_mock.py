be07184c2a39f972285db282905775acacb1c8c2
Daniel Campos <danielcampos14@hotmail.com.br>
2024-09-30 19:23:18 -0300

    test: test item prices

--- backend/domain/tests/domain/mocks/item_repo_mock.py
+++ backend/domain/tests/domain/mocks/item_repo_mock.py
@@ -1,10 +1,31 @@
 from domain.repository.itemRepo import IItemRepo
+from datetime import datetime
+
 
 class ItemRepoMock(IItemRepo):
+
     def get_item_info(self, item_id: int) -> dict | None:
         if not isinstance(item_id, int):
             return dict()
         return dict()
-    
+
     def get_item_prices(self, item_id: int) -> dict | None:
-        return super().get_item_prices(item_id)
+        if not isinstance(item_id, int):
+            return dict()
+
+        today_day = datetime.today().day
+        today_month = datetime.today().month
+        today_year = datetime.today().year
+        today_datetime = datetime(today_year, today_month, today_day)
+        # The API timecode comes in miliseconds, but today_datetime.timestamp
+        # is in seconds
+        today_timestamp = str(today_datetime.timestamp() * 1000).split(".")[0]
+        items_prices = {
+            'daily': {
+                today_timestamp: 1
+            },
+            'average': {
+                today_timestamp: 2
+            }
+        }
+        return items_prices
--- backend/domain/tests/domain/test_item_service.py
+++ backend/domain/tests/domain/test_item_service.py
@@ -1,9 +1,11 @@
 from unittest import main, TestCase
 from domain.itemServiceImp import ItemServiceImp
 from domain.tests.domain.mocks.item_repo_mock import ItemRepoMock
+from datetime import datetime
 
 
 class TestItemService(TestCase):
+
     def setUp(self):
         self.item_service = ItemServiceImp(ItemRepoMock())
 
@@ -11,11 +13,24 @@ class TestItemService(TestCase):
         item_id = 1
         item_info = self.item_service.get_item_info(item_id)
         self.assertIsInstance(item_info, dict)
-    
-    def test_get_empty_dict_invalid_id(self):
+
+    def test_get_empty_dict_invalid_id_item_info(self):
         item_id = 'a'
         item_info = self.item_service.get_item_info(item_id)
         self.assertDictEqual(item_info, dict())
+
+    def test_get_empty_dict_invalid_id_item_prices(self):
+        item_id = 'a'
+        item_prices = self.item_service.get_item_prices(item_id)
+        self.assertDictEqual(item_prices, dict())
+
+    def test_get_item_price_date_as_date(self):
+        item_id = 1
+        item_prices = self.item_service.get_item_prices(item_id)
+        curr_date = datetime.now().date()
+        self.assertEqual(list(item_prices['daily'].keys())[0], curr_date)
+        self.assertEqual(list(item_prices['average'].keys())[0], curr_date)
+
 
 if __name__ == "__main__":
     main()
